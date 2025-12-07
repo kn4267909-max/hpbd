2dc41c47001076507d8e0e52dc665ab8dc9eb6f3
khoinguyen162 <kn4267909@gmail.com>
2025-12-07 12:52:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,15 @@
-export const PASSWORD = "12345"; //sửa pass ở đây
+export const PASSWORD = "1234"; //sửa pass ở đây
 
 //sửa phần text vs btn
-
 export const noData = [
-
     { text: "Anh chắc chưa đó?", btn: "Thật sao anh", img: "./assets/images/2.gif" },
-
     { text: "Cục cưng ơi", btn: "Anh chắc chưa đó?", img: "./assets/images/3.gif" },
-
     { text: "Đừng làm vậy với em", btn: "Đừng làm vậy với Em", img: "./assets/images/4.gif" },
-
     { text: "Tim em đang tan vỡ", btn: "Tim em đang tan vỡ", img: "./assets/images/5.gif" },
-
     { text: "Em sắp khóc rồi đó", btn: "Em sắp khóc rồi đó", img: "./assets/images/6.gif" }
-
 ];
 
 //sửa phần thư
 export const letterHeading = "Gửi em,";
-export const letterText = Cảm ơn em đã bước đến một cách mãnh liệt nhưng cũng dịu dàng đến thế.;
+
+export const letterText = `Cảm ơn em đã bước đến một cách mãnh liệt nhưng cũng dịu dàng đến thế.`;
